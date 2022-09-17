a256d016c79f811208acbabd100791955a003153
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-16 23:22:35 -0300

    Actualización

---
+++ src/Componentes/Contador.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+  let unidad = props.segundos;
+  let decena = 0;
+  let centena = 0;
+  let millar = 0;
+  let diezMillar = 0;
+  let cienCentena = 0;
+};
