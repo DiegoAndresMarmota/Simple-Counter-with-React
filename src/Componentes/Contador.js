66d5dc9626133d0fb372336b24607a846737d73e
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-16 23:29:38 -0300

    Props

--- src/Componentes/Contador.js
+++ src/Componentes/Contador.js
@@ -1,10 +1,29 @@
 import React from "react";
 
 const SecondsCounter = (props) => {
-  let unidad = props.segundos;
-  let decena = 0;
-  let centena = 0;
-  let millar = 0;
-  let diezMillar = 0;
-  let cienCentena = 0;
-};
+  let unidad = props.seconds;
+  let diez = 0;
+  let cien = 0;
+  let mil = 0;
+  let diezMil = 0;
+  let cienMil = 0;
+  if (props.seconds > 9) {
+    diez = Math.trunc(props.seconds / 10);
+    unidad = props.seconds % 10;
+    if (diez > 9) {
+      cien = Math.trunc(diez / 10);
+      diez = diez % 10;
+      if (cien > 9) {
+        mil = Math.trunc(cien / 10);
+        cien = cien % 10;
+        if (mil > 9) {
+          diezMil = Math.trunc(mil / 10);
+          mil = mil % 10;
+          if (diezMil > 9) {
+            cienMil = Math.trunc(diezMil / 10);
+            diezMil = diezMil % 10;
+          }
+        }
+      }
+    }
+  }
