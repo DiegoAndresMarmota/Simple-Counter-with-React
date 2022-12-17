c55c737c22bf481c957390f91dbbd9964146437e
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-17 02:20:58 -0300

    Modificación del index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import Home from "./component/home.jsx";
 import "./index.css";
-import { Contador } from "./Contador";
 
-let contador = 0;
+let counter1 = -1;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+let counter5 = 0;
 
-setInterval(function () {
+setInterval(() => {
+  counter1++;
+  counter1 == 10 ? ((counter1 = 0), counter2++) : true;
+  counter2 == 10 ? ((counter2 = 0), counter3++) : true;
+  counter3 == 10 ? ((counter3 = 0), counter4++) : true;
+  counter4 == 10 ? ((counter4 = 0), counter5++) : true;
+  counter5 == 10 ? (counter5 = 0) : true;
+
+  console.log(counter1, counter2, counter3, counter4, counter5);
+
   ReactDOM.render(
-    <Contador segundos={contador} />,
-    document.getElementById("root")
+    <Home
+      fifth={counter5}
+      fourth={counter4}
+      third={counter3}
+      second={counter2}
+      first={counter1}
+    />,
+    document.querySelector("#app")
   );
-  contador += 1;
 }, 1000);
