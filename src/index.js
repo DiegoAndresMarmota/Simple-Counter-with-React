5bc5c3a87dbbe26f7cd08bf7dc42a77cb6d4b15c
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-21 21:36:53 -0300

    Corrección del counter

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ let counter5 = 0;
 
 setInterval(() => {
   counter1++;
-  counter1 == 10 ? ((counter1 = 0), counter2++) : true;
-  counter2 == 10 ? ((counter2 = 0), counter3++) : true;
-  counter3 == 10 ? ((counter3 = 0), counter4++) : true;
-  counter4 == 10 ? ((counter4 = 0), counter5++) : true;
-  counter5 == 10 ? (counter5 = 0) : true;
+  // counter1 == 10 ? ((counter1 = 0), counter2++) : true;
+  // counter2 == 10 ? ((counter2 = 0), counter3++) : true;
+  // counter3 == 10 ? ((counter3 = 0), counter4++) : true;
+  // counter4 == 10 ? ((counter4 = 0), counter5++) : true;
+  // counter5 == 10 ? (counter5 = 0) : true;
 
   console.log(counter1, counter2, counter3, counter4, counter5);
 
